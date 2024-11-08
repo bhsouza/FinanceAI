d6e2d4ead4ce9112f3ead6a10fdb229daf15fb6a
Breno Henrique <breno.hs@outlook.com>
2024-11-08 11:46:32 -0300

    feat: ensure transactions and subscriptions pages are protected

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -3,10 +3,18 @@ import { DataTable } from "../_components/ui/data-table";
 import { TransactionColumns } from "./_columns";
 import AddTransactionButton from "../_components/add-transaction-button";
 import Navbar from "../_components/navbar";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 const TransactionsPage = async () => {
   // Acessar as transacoes do banco de dados
-  const transactions = await db.transaction.findMany({});
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
+  const transactions = await db.transaction.findMany({
+    where: { userId },
+  });
   return (
     <>
       <Navbar />
